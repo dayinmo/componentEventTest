0285a2c51be63b715b109148a5a180e4918de1cb
zhufu <yons@iMac.local>
2018-06-19 17:12:36 +0800

    commit

---
+++ components/component-tag-name.js
@@ -0,0 +1,11 @@
+// components/component-tag-name.js
+Component({
+  properties: {},
+  methods: {
+    onTap: function () {
+      var myEventDetail = {} // detail对象，提供给事件监听函数
+      var myEventOption = {} // 触发事件的选项
+      this.triggerEvent('myevent', myEventDetail, myEventOption)
+    }
+  }
+})
